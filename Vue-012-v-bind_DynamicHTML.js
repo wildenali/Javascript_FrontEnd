e3dea14d4cfa40fc3f641c70455e2c2383132306
wildenali <wildeeeeen@gmail.com>
2020-06-17 11:27:51 +0700

    cara pakai v-bind di dynamic html

---
+++ Vue-012-v-bind_DynamicHTML.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    // key: value
+    el: '#app', // el itu element
+    data: {
+        imgFile: 'img/gambar1.jpg',
+        linkINI: 'Vue-012-about.html',
+        styleHuruf: 'color: red; fontSize: 50px',
+    },
+});
