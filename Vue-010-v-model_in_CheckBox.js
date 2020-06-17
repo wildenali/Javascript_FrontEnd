cbc78b78164e2fdd29d697cb56071491054aaae9
wildenali <wildeeeeen@gmail.com>
2020-06-17 11:26:12 +0700

    cara pakai v-model di checkbox

---
+++ Vue-010-v-model_in_CheckBox.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    // key: value
+    el: '#app', // el itu element
+    data: {
+        pesan1: '',
+        iniTextarea: '',
+        iniRadioButton: '',
+        iniCheckBox: [],
+    },
+});
