f7bce99cf5f472961961ec8f7ee384ff8bab2570
wildenali <wildenwildenaliali@gmail.com>
2020-06-17 23:30:42 +0700

    coba framework vue.js

---
+++ VueLogosProject/hello-world/src/components/Logos.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div class="logos-wrapper">
+            <h2 class="title-header">Cek Logos</h2>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/burger1.png">
+            </div>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/burger2.png">
+            </div>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/burger3.png">
+            </div>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/burger4.png">
+            </div>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/burger5.png">
+            </div>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/burger6.png">
+            </div>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/burger7.png">
+            </div>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/burger8.png">
+            </div>
+            <div class="item-logos">
+                <img class="logo" alt="Burger1 logo" src="../assets/martabak.png">
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'Logos'
+}
+</script>
+
+<style scoped>
+.title-header {
+    margin-bottom: 80px;
+}
+
+.logos-wrapper {
+    width: 52%;
+    overflow: hidden;
+    margin: auto;
+}
+
+.item-logos {
+    float: left;
+    margin-left: 70px;
+    margin-bottom: 70px;
+    width: 150px;
+    height: 150px;
+}
+.item-logos .logo {
+    width: 150px;
+    height: 150px;
+}
+</style>
