96a33c36bbc80a2b7d0b676d98126f33650629eb
wildenali <wildeeeeen@gmail.com>
2020-06-17 11:22:17 +0700

    cara pakai v-model di textarea

---
+++ Vue-008-v-model_in_Textarea.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    // key: value
+    el: '#app', // el itu element
+    data: {
+        pesan1: '',
+        iniTextarea: '',
+    },
+});
+
+
+// Memiliki fitur watch
+// syaratnya data properti yg akan di ubah harus sama
